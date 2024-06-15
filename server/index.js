ccc5d193bc5b27dd64e72d34b1f3058a8686fee4
prashantghadiali <er.prashantghadiali@gmail.com>
2024-06-15 16:28:07 +0530

    server code

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const connectDB = require('./config/db')
+const router = require('./routes')
+
+
+const app = express()
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api",router)
+
+const PORT = 8080 || process.env.PORT
+
+
+connectDB().then(()=>{
+    app.listen(PORT,()=>{
+        console.log("connnect to DB")
+        console.log("Server is running "+PORT)
+    })
+})
